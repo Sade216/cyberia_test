152391a38f53caffc117cc5a1585285389e1a7e6
sade216 <vlad.tok.1997@mail.ru>
2025-02-25 01:15:47 +0700

    lint settings

--- src/services/setiings.ts
+++ src/services/setiings.ts
@@ -1,29 +1,28 @@
 import axios from "axios";
 
-export function SetupAxios(){
-    axios.defaults.baseURL = process.env.API_URL
+export function SetupAxios() {
+    axios.defaults.baseURL = process.env.API_URL;
 }
-
 
 export const paths = [
     {
-        path: '/agency',
-        lable: 'Агенство'
+        path: "/agency",
+        lable: "Агенство",
     },
     {
-        path: '/services',
-        lable: 'Услуги'
+        path: "/services",
+        lable: "Услуги",
     },
     {
-        path: '/cases',
-        lable: 'Кейсы'
+        path: "/cases",
+        lable: "Кейсы",
     },
     {
-        path: '/blog',
-        lable: 'Блог'
+        path: "/blog",
+        lable: "Блог",
     },
     {
-        path: '/contacts',
-        lable: 'Контакты'
-    }
-]
+        path: "/contacts",
+        lable: "Контакты",
+    },
+];
