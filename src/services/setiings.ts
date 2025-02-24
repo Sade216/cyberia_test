3242912f5dfa1d3a779deac6320bdd1bdea75944
sade216 <vlad.tok.1997@mail.ru>
2025-02-24 23:47:15 +0700

    настройки для axios и глобальные роуты

---
+++ src/services/setiings.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export function SetupAxios(){
+    axios.defaults.baseURL = process.env.API_URL
+}
+
+
+export const paths = [
+    {
+        path: '/agency',
+        lable: 'Агенство'
+    },
+    {
+        path: '/services',
+        lable: 'Услуги'
+    },
+    {
+        path: '/cases',
+        lable: 'Кейсы'
+    },
+    {
+        path: '/blog',
+        lable: 'Блог'
+    },
+    {
+        path: '/contacts',
+        lable: 'Контакты'
+    }
+]
