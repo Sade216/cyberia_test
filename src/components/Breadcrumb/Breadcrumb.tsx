952692052bdc7bda8deea01dc8bc72b7affea5dd
sade216 <vlad.tok.1997@mail.ru>
2025-02-26 20:34:07 +0700

    Добавлен автономный breadcrumb

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,11 +1,65 @@
-import React from 'react'
+"use client";
 
-import styles from './Breadcrumb.module.css'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import styles from "./Breadcrumb.module.css";
 
-function Breadcrumb() {
-  return (
-    <aside className={styles.Wrapper}>Главная / Кейсы</aside>
-  )
-}
+import { paths } from "@/services/setiings";
 
-export default Breadcrumb
+const Breadcrumb = () => {
+    const pathname = usePathname();
+
+    const getBreadcrumbs = (pathname: string) => {
+        const segments = pathname.split("/").filter(Boolean);
+        let currentPath = "/";
+
+        return segments.map((segment) => {
+            currentPath += `${segment}`;
+
+            const pathMatch = Object.values(paths).find(
+                (path) => path.path === currentPath
+            );
+
+            return {
+                label: pathMatch
+                    ? pathMatch.label
+                    : segment.charAt(0).toUpperCase() + segment.slice(1),
+                href: currentPath,
+            };
+        });
+    };
+
+    const breadcrumbs = getBreadcrumbs(pathname);
+
+    return (
+        <nav
+            aria-label="breadcrumb"
+            className={styles.Wrapper}>
+            <ol className={styles.Breadcrumb}>
+                <li className={styles.BreadcrumbItem}>
+                    <Link
+                        href="/"
+                        className={styles.BreadcrumbLinkMain}>
+                        Главная
+                    </Link>
+                </li>
+                {breadcrumbs.map((breadcrumb, index) => (
+                    <li
+                        key={index}
+                        className={styles.BreadcrumbItem}>
+                        <>
+                            <span className={styles.Separator}> / </span>
+                            <Link
+                                href={breadcrumb.href}
+                                className={styles.BreadcrumbLink}>
+                                {breadcrumb.label}
+                            </Link>
+                        </>
+                    </li>
+                ))}
+            </ol>
+        </nav>
+    );
+};
+
+export default Breadcrumb;
