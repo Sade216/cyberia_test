7fb04387e178181b30d0f75b129bae8e03cdcaaf
sade216 <vlad.tok.1997@mail.ru>
2025-02-28 23:32:29 +0700

    Добавлены переиспользуемые иконки

---
+++ src/components/Icons/CornersIcon.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function CornersIcon() {
+    return (
+        <svg
+            width="25"
+            height="25"
+            viewBox="0 0 25 25"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg">
+            <rect
+                width="24.9999"
+                height="5.46874"
+                rx="1"
+                transform="matrix(1 0 0 -1 0 24.9995)"
+                fill="currentColor"
+            />
+            <rect
+                width="14.0625"
+                height="5.46874"
+                rx="1"
+                transform="matrix(1 0 0 -1 10.938 14.0625)"
+                fill="currentColor"
+            />
+            <rect
+                width="24.9999"
+                height="5.46874"
+                rx="1"
+                transform="matrix(0 1 1 0 0 0.00012207)"
+                fill="currentColor"
+            />
+            <rect
+                width="14.0625"
+                height="5.46874"
+                rx="1"
+                transform="matrix(0 1 1 0 10.938 0.00012207)"
+                fill="currentColor"
+            />
+        </svg>
+    );
+}
+
+export default CornersIcon;
