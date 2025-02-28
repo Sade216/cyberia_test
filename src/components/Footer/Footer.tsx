c677fa560292254aa5fa2c0f8a6c4f1db842a47b
sade216 <vlad.tok.1997@mail.ru>
2025-02-28 23:24:27 +0700

    Обновлён footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,24 +1,33 @@
 import React from "react";
+import Link from "next/link";
+import { paths } from "@/services/setiings";
 
+import Container from "../Container/Container";
 import styles from "./Footer.module.css";
-import Logo from "../Logo/Logo";
+import Logo from "../Icons/LogoIcon";
 
 function Footer() {
     return (
         <footer className={styles.Wrapper}>
-            <div>
-                <Logo />
-                <div>Веб-разработка и усиление IT-команд</div>
-            </div>
-            <div>
-                <div>+7 999 123 45 67</div>
-                <div>hello@cyberia.studio</div>
-                <div>ул.Ярных, д.35, оф.10</div>
-                <div>hello@cyberia.studio</div>
-                <div>hello@cyberia.studio</div>
-                <div>hello@cyberia.studio</div>
-                <div>hello@cyberia.studio</div>
-            </div>
+            <Container className={styles.Container}>
+                <div className={styles.Promo}>
+                    <Logo />
+                    <div>Веб-разработка и усиление IT-команд</div>
+                </div>
+                <div className={styles.Grid}>
+                    <div>+7 999 123 45 67</div>
+                    <div>hello@cyberia.studio</div>
+                    <div>ул.Ярных, д.35, оф.10</div>
+                    {Object.entries(paths).map(([key, { label, path }]) => (
+                        <Link
+                            className={styles.Link}
+                            key={key}
+                            href={path}>
+                            {label}
+                        </Link>
+                    ))}
+                </div>
+            </Container>
         </footer>
     );
 }
