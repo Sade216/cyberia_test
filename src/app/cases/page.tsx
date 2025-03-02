96e9bd600d21ae840a8770b04a9c9984e8b3ba6f
sade216 <vlad.tok.1997@mail.ru>
2025-03-03 01:23:12 +0700

    обновлена страница кейсы

--- src/app/cases/page.tsx
+++ src/app/cases/page.tsx
@@ -1,7 +1,19 @@
+import Container from "@/components/Container/Container";
 import styles from "./Cases.module.css";
+import Projects from "./components/Projects/Projects";
+import FeedbackForm from "./components/FeedbackForm/FeedbackForm";
 
 function Cases() {
-    return <div className={styles.Wrapper}>Cases</div>;
+
+    return (
+        <main>
+            <Container forPage className={styles.Wrapper}>
+                <h1>Кейсы</h1>
+                <Projects/>
+                <FeedbackForm/>
+            </Container>
+        </main>
+    );
 }
 
 export default Cases;
