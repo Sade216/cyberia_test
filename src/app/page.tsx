1ff928c69625c676881acd740fc4a52ca23914b2
sade216 <vlad.tok.1997@mail.ru>
2025-03-03 03:09:42 +0700

    обновление главной страницы

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import Container from "@/components/Container/Container";
 import Link from "next/link";
+
+import Container from "@/components/Container/Container";
+import styles from './Main.module.css'
 
 export default function Home() {
     return (
         <main>
-            <Container forPage>
+            <Container className={styles.Wrapper} forPage>
                 <h1>Главная</h1>
-                <Link href="/cases">Кейсы</Link>
+                <Link className={styles.Link} href="/cases">Кейсы</Link>
             </Container>
         </main>
     );
