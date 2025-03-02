cbc5c0e4e2d19664b1e3d9801bd77cc86777200c
sade216 <vlad.tok.1997@mail.ru>
2025-03-03 01:20:00 +0700

    добавлен компонен карточки проекта

---
+++ src/app/cases/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { TProject } from "@/services/types";
+
+import styles from "./ProjectCard.module.css";
+import Image from "next/image";
+import CornersIcon from "@/components/Icons/CornersIcon";
+
+type TCardProps = {
+    data: TProject;
+    className?: string;
+};
+
+function ProjectCard({ className, data }: TCardProps) {
+    return (
+        <section className={`${className && className} ${styles.Card}`}>
+            <Image
+                loader={() => data.image}
+                src={data.image}
+                alt="Food Image"
+                width={300}
+                height={300}
+                unoptimized
+                priority={false}
+            />
+            <div className={styles.Overlay}></div>
+            <div className={styles.Content}>
+                <CornersIcon />
+                <h3 className={styles.Title}>{data.title}</h3>
+                <p className={styles.Description}>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Totam consequatur itaque corporis doloremque.
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default ProjectCard;
