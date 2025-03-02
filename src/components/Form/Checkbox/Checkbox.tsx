e971c5536931c8f7f09bccc911455f84a62d1089
sade216 <vlad.tok.1997@mail.ru>
2025-03-03 01:17:34 +0700

    добавлен самостоятельный ui компонент checkbox

---
+++ src/components/Form/Checkbox/Checkbox.tsx
@@ -0,0 +1,34 @@
+import React, { Ref } from "react";
+
+import styles from "./Checkbox.module.css";
+
+type TCheckboxProps = {
+    id: string;
+    labelText?: string;
+    className?: string;
+    ref?: Ref<HTMLInputElement>;
+    disabled?: boolean,
+};
+
+function Checkbox(props: TCheckboxProps) {
+    return (
+        <div className={`${styles.Wrapper} ${props.className && props.className}`}>
+            <input
+                className={styles.Checkbox}
+                type="checkbox"
+                id={props.id}
+                ref={props.ref}
+                disabled={props.disabled}
+            />
+            {props.labelText && (
+                <label
+                    className={styles.CheckboxLabel}
+                    htmlFor={props.id}>
+                    {props.labelText}
+                </label>
+            )}
+        </div>
+    );
+}
+
+export default Checkbox;
